80f4e9f4122921207376e9b9f5b8075e5faa021e
RT <rotyflo@gmail.com>
2019-08-19 21:08:58 -0700

    Print output from different operations on numbers

---
+++ intro_to_programming/numbers.rb
@@ -0,0 +1,17 @@
+# Write 4 examples of expressions using each of the operations: 
+# +, -, /, and %. Print out the result of your expressions using puts. 
+
+puts 7 + 4 # 11
+puts 7 - 4 # 3
+puts 7 / 4 # 1
+puts 7 % 4 # 3
+
+# Write 6 comparison expressions using each of the operations:
+# ==, !=, <, >, <=, >=. Print out the result of your expressions using puts.
+
+puts 7 == 4 # false
+puts 7 != 4 # true
+puts 7 < 4 # false
+puts 7 > 4 # true
+puts 7 <= 4 # false
+puts 7 >= 4 # true
