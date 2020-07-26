a5e0d6d86e0443032200d83d44b34323351b481c
RT <rotyflo@gmail.com>
2020-07-26 11:40:10 -0700

    Complete algorithmic strategies exercises

---
+++ software_engineering_foundations/scope_and_references/algorithmic_strategies_exercise/05_compress_str.rb
@@ -0,0 +1,27 @@
+# Write a method, compress_str(str), that accepts a string as an arg.
+# The method should return a new str where streaks of consecutive characters are compressed.
+# For example "aaabbc" is compressed to "3a2bc".
+def compress_str(str)
+  new_str = ""
+
+  count = 0
+  (0...str.length).each do |i|
+    if str[i] == str[i + 1]
+      count += 1
+    elsif count > 0
+      count += 1
+      new_str += count.to_s + str[i]
+      count = 0
+    else
+      new_str += str[i]
+    end
+  end
+
+  new_str
+end
+
+
+p compress_str("aaabbc")        # => "3a2bc"
+p compress_str("xxyyyyzz")      # => "2x4y2z"
+p compress_str("qqqqq")         # => "5q"
+p compress_str("mississippi")   # => "mi2si2si2pi"
