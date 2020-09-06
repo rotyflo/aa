ed80c1f479c13124d2acf880873783b7a38266dd
RT <rotyflo@gmail.com>
2020-09-06 14:48:08 -0700

    Finish hangman game

--- software-engineering-foundations/input-output/hangman/lib/hangman.rb
+++ software-engineering-foundations/input-output/hangman/lib/hangman.rb
@@ -39,4 +39,56 @@ class Hangman
   def fill_indices(char, indices)
     indices.each { |i| @guess_word[i] = char }
   end
+
+  def try_guess(char)
+    if already_attempted?(char)
+      p 'that has already been attempted'
+      return false
+    else
+      matching_indices = get_matching_indices(char)
+      @attempted_chars << char
+      
+      if matching_indices.empty?
+        @remaining_incorrect_guesses -= 1
+      else
+        fill_indices(char, matching_indices)
+      end
+      
+      return true
+    end
+  end
+
+  def ask_user_for_guess
+    p 'Enter a char: '
+    char = gets.chomp
+
+    try_guess(char)
+  end
+
+  def win?
+    if @guess_word.join == @secret_word
+      p 'WIN'
+      return true
+    else
+      return false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      p 'LOSE'
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    if win? or lose?
+      p @secret_word
+      return true
+    else
+      return false
+    end
+  end
 end
