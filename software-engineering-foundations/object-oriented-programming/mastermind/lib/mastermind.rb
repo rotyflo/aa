4b72e83d708e749c7b2bb4c49f14f2908856bb9e
RT <rotyflo@gmail.com>
2020-09-10 12:57:46 -0700

    Finish mastermind part 3

--- software-engineering-foundations/object-oriented-programming/mastermind/lib/mastermind.rb
+++ software-engineering-foundations/object-oriented-programming/mastermind/lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    p 'Exact matches: ' + @secret_code.num_exact_matches(code).to_s
+    p 'Near matches: ' + @secret_code.num_near_matches(code).to_s
+  end
+
+  def ask_user_for_guess
+    p 'Enter a code'
+    code = Code.from_string(gets.chomp)
+    print_matches(code)
+    code == @secret_code
+  end
 end
