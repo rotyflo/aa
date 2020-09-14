46255d454d85e4d6964d259dd8f6a199d47c731a
RT <rotyflo@gmail.com>
2020-09-14 09:05:44 -0700

    Finish battleship part 2

--- software-engineering-foundations/object-oriented-programming/battleship/lib/board.rb
+++ software-engineering-foundations/object-oriented-programming/battleship/lib/board.rb
@@ -1,4 +1,8 @@
 class Board
+    def self.print_grid(grid)
+        grid.each { |row| puts row.join(' ') }
+    end
+
     attr_reader :size
 
     def initialize(n)
@@ -17,12 +21,44 @@ class Board
     end
 
     def num_ships
-        count = 0
-        (0...@grid.length).each do |x|
-            (0...@grid.length).each do |y|
-                count += 1 if @grid[x][y] == :S
+        @grid.flatten.count { |ele| ele == :S }
+    end
+
+    def attack(pos)
+        if self[pos] == :S
+            self[pos] = :H
+            p 'You sunk my battleship!'
+            return true
+        end
+        
+        self[pos] = :X
+        false
+    end
+
+    def place_random_ships
+        ships = 0
+
+        while ships < @size / 4
+            rand_pos = [rand(@grid.length - 1), rand(@grid.length - 1)]
+
+            if self[rand_pos] == :N
+                self[rand_pos] = :S
+                ships += 1
             end
         end
-        count
+    end
+
+    def hidden_ships_grid
+        @grid.map do |row|
+            row.map { |pos| pos == :S ? :N : pos }
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(hidden_ships_grid)
     end
 end
