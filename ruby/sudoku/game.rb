dd053bacc00aaf3dba0c65853230ab09fab3a5d3
RT <rotyflo@gmail.com>
2020-10-05 20:50:28 -0700

    Add introduction to game

--- ruby/sudoku/game.rb
+++ ruby/sudoku/game.rb
@@ -6,6 +6,7 @@ class Game
 	end
 
 	def play
+		greet
 		until @board.solved?
 			@board.render
 			tile = @board.select_tile
@@ -14,6 +15,21 @@ class Game
 		end
 		@board.render
 		puts "You win!"
+	end
+
+	def greet
+		system("clear")
+		puts "Welcome to Sudoku!"
+		puts "\nHOW TO SOLVE:"
+		puts "In order to complete this puzzle, each row, column and 3x3 grid"
+		puts "must contain only 1 instance of each number 1 through 9."
+		puts "\nHOW TO PLAY:"
+		puts "Enter the indices of a location without space between them."
+		puts "\teg. Position: 23"
+		puts "\nEnter a single digit between 0 and 9."
+		puts "\teg. Number: 8"
+		puts "\nPress 'enter' to start."
+		gets
 	end
 
 	def get_value
