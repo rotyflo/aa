d94ec91cbf16f311af92556c214a37c853d08b40
RT <rotyflo@icloud.com>
2020-10-21 11:16:06 -0700

    Make better change

--- ruby/projects/recursion/make_change.rb
+++ ruby/projects/recursion/make_change.rb
@@ -5,7 +5,25 @@ def greedy_make_change(amount, coins = [25, 10, 5, 1])
 	[coins.last] + greedy_make_change(amount - coins.last, coins)
 end
 
-p greedy_make_change(120)
-p greedy_make_change(99)
-p greedy_make_change(24)
-p greedy_make_change(0)
+p greedy_make_change(120) # [25, 25, 25, 25, 10, 10]
+p greedy_make_change(99) # [25, 25, 25, 10, 10, 1, 1, 1, 1]
+
+# Compare every combination of change
+def make_better_change(amount, coins = [25, 10, 5, 1])
+	return [] if amount < 1
+	smallest_change = nil
+
+	coins.sort.reverse.each.with_index do |coin, i|
+		unless coin > amount
+			change = [coin] + make_better_change(amount - coin, coins.drop(i))
+			if smallest_change == nil || change.count < smallest_change.count
+				smallest_change = change
+			end
+		end
+	end
+
+	smallest_change
+end
+
+p greedy_make_change(24, [10, 7, 1]) # [10, 10, 1, 1, 1, 1]
+p make_better_change(24, [10, 7, 1]) # [10, 7, 7]
